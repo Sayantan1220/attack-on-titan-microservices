ba0ea33618cd77bfaf0e619d5101df9ba4daca9a
Sayantan <sayantan.das208@gmail.com>
2023-05-07 19:47:59 +0530

    Titan service

---
+++ titan-service/src/test/java/com/microservices/titanservice/TitanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.titanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
